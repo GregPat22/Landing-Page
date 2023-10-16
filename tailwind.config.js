33fec53f4dd892160f613d28295e096ff5094818
Greg <ninopatini@icloud.com>
2023-10-16 18:45:38 +0200

    added Flowbite and added a dropdown menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./planets/*.{html,js}", "./index.html"],
+  content: [
+    "./planets/*.{html,js}",
+    "./index.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 
 // npx tailwindcss -i ./landing_style/input.css -o ./dist/output.css --watch
