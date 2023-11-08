6fa5b67c3bea1e37041dc661ea695d825986f8d5
Greg <ninopatini@icloud.com>
2023-11-08 12:04:18 +0100

    create a js file for the dropdown menu

--- planets/marscript.js
+++ planets/marscript.js
@@ -44,3 +44,71 @@ themeToggleBtn.addEventListener("click", function () {
 // --------------------------------------------------------------------------
 
 // nav bar toggle menu
+class Collapse {
+  constructor(targetEl, triggerEl, options = {}) {
+    this.targetElement = targetEl;
+    this.triggerElement = triggerEl;
+    this.options = {
+      transition: options.transition || "height 0.3s ease",
+      duration: options.duration || 300,
+      timingFunction: options.timingFunction || "ease",
+      onExpand: options.onExpand || null,
+      onCollapse: options.onCollapse || null,
+    };
+
+    this.isCollapsed = true;
+
+    this.init();
+  }
+
+  init() {
+    this.triggerElement.addEventListener("click", () => this.toggle());
+    this.updateStyle();
+  }
+
+  toggle() {
+    this.isCollapsed = !this.isCollapsed;
+    this.updateStyle();
+
+    if (this.isCollapsed) {
+      if (this.options.onCollapse) {
+        this.options.onCollapse(this.targetElement);
+      }
+    } else {
+      if (this.options.onExpand) {
+        this.options.onExpand(this.targetElement);
+      }
+    }
+  }
+
+  updateStyle() {
+    const { transition, duration, timingFunction } = this.options;
+    this.targetElement.style.transition = transition;
+    this.targetElement.style.transitionDuration = `${duration}ms`;
+    this.targetElement.style.transitionTimingFunction = timingFunction;
+
+    if (this.isCollapsed) {
+      this.targetElement.style.height = "0";
+    } else {
+      this.targetElement.style.height = this.targetElement.scrollHeight + "px";
+    }
+  }
+}
+
+// Example usage:
+const triggerButton = document.getElementById("triggerButton");
+const targetElement = document.getElementById("targetElement");
+
+const collapseOptions = {
+  transition: "height 0.5s ease",
+  duration: 500,
+  timingFunction: "ease",
+  onExpand: (element) => {
+    console.log("Expanded:", element);
+  },
+  onCollapse: (element) => {
+    console.log("Collapsed:", element);
+  },
+};
+
+const myCollapse = new Collapse(targetElement, triggerButton, collapseOptions);
