6a5d2f67288331864fc2a78e040670e6b321a4bf
Greg <ninopatini@icloud.com>
2023-11-13 17:49:09 +0100

    all navbar problems fixed and dropdown's too

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
   variants: {
